25801db05ab24d7774fb3430daddea83e03b99ed
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-12-25 22:55:26 +0100

    Add files via upload

---
+++ CPP_Module_01/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+int main() {
+    int N = 3;
+    std::string name = "Zombie";
+
+    Zombie* horde = zombieHorde(N, name);
+
+    for (int i = 0; i < N; i++) {
+        horde[i].announce();
+    }
+
+    delete[] horde;
+
+    return 0;
+}
