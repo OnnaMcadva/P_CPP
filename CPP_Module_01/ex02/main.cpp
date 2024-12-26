945096fd7a5992e221bcdc243a27875233a02fb7
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-12-26 23:50:29 +0100

    Add files via upload

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#define DEF_COLOR "\033[0;39m"
+#define RED "\033[0;91m"
+#define ORANGE "\033[38;5;214m"
+#define YELLOW "\033[0;33m"
+#define GREEN "\033[0;92m"
+#define BLUE "\033[0;94m"
+#define INDIGO "\033[38;5;54m"
+#define VIOLET "\033[38;5;129m"
+
+int main() {
+
+    std::string str = "HI THIS IS BRAIN";
+
+    std::string* stringPTR = &str;
+
+    std::string& stringREF = str;
+
+    std::cout << RED "Memory address of the string variable: " DEF_COLOR << &str << std::endl;
+    std::cout << ORANGE "Memory address held by stringPTR: " DEF_COLOR << stringPTR << std::endl;
+    std::cout << YELLOW "Memory address held by stringREF: " DEF_COLOR << &stringREF << std::endl;
+
+    std::cout << GREEN "Value of the string variable: " DEF_COLOR << str << std::endl;
+    std::cout << BLUE "Value pointed to by stringPTR: " DEF_COLOR << *stringPTR << std::endl;
+    std::cout << INDIGO "Value pointed to by stringREF: " DEF_COLOR << stringREF << std::endl;
+
+    return 0;
+}
