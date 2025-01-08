5f03bb01c269bf1a81ea802abfb9735fd1b5123f
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-01-08 23:18:06 +0100

    Add files via upload

--- CPP_Module_03/ex00/main.cpp
+++ CPP_Module_03/ex00/main.cpp
@@ -1,10 +1,10 @@
-#include "ClapTrap.hpp"
-#include <sstream>
+# include "ClapTrap.hpp"
+# include <sstream>
 
 int main() {
 
-    ClapTrap clap1("Clappy");
-    ClapTrap clap2("Trappy");
+    ClapTrap clap1("\033[32mClappy\033[0m");
+    ClapTrap clap2("\033[33mTrappy\033[0m");
 
     std::cout << "\n--- Testing Attack ---" << std::endl;
     clap1.attack("Target 1");
@@ -21,10 +21,10 @@ int main() {
     clap2.beRepaired(5);
 
     std::cout << "\n--- Testing Energy Depletion ---" << std::endl;
-    ClapTrap noEnergy("NoEnergy");
+    ClapTrap noEnergy("\033[34mNoEnergy\033[0m");
     for (int i = 0; i < 11; i++) {
         std::stringstream ss;
-        ss << "Dummy Target " << (i + 1);
+        ss << "\033[36mDummy Target\033[0m " << (i + 1);
         noEnergy.attack(ss.str());
     }
     noEnergy.beRepaired(10);
@@ -34,7 +34,7 @@ int main() {
     clapCopy.attack("Copied Target");
 
     std::cout << "\n--- Testing Exhausted ClapTrap ---" << std::endl;
-    ClapTrap exhausted("Exhausted");
+    ClapTrap exhausted("\033[35mExhausted\033[0m");
     exhausted.takeDamage(10);
     exhausted.attack("Nobody");
     exhausted.beRepaired(5);
