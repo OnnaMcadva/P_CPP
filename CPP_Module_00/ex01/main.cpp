b1019dca1ce6cde2943da2f7f040b7b1c1236fe2
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-01-13 16:19:42 +0100

    Update main.cpp

--- CPP_Module_00/ex01/main.cpp
+++ CPP_Module_00/ex01/main.cpp
@@ -10,21 +10,21 @@ int main() {
 
     std::cout << "Welcome to My Awesome PhoneBook!\n";
     while (true) {
-    std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-    if (!std::getline(std::cin, command)) {  // Проверка состояния std::cin
-        std::cout << "\nInput terminated. Exiting program.\n";
-        break;  // Выход из цикла при EOF
-    }
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+        if (!std::getline(std::cin, command)) {  // Проверка состояния std::cin
+            std::cout << "\nInput terminated. Exiting program.\n";
+            break;  // Выход из цикла при EOF
+        }
 
-    if (command == "ADD") {
-        phoneBook.addContact();
-    } else if (command == "SEARCH") {
-        phoneBook.searchContact();
-    } else if (command == "EXIT") {
-        break;
-    } else {
-        std::cout << "Invalid command!\n";
-    }
+        if (command == "ADD") {
+            phoneBook.addContact();
+        } else if (command == "SEARCH") {
+            phoneBook.searchContact();
+        } else if (command == "EXIT") {
+            break;
+        } else {
+            std::cout << "Invalid command!\n";
+        }
     }
 
     std::cout << "Goodbye!\n";
