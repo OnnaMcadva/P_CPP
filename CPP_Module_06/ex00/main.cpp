69422342f2cce91080583f3de730500b07ca665f
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-02-06 21:27:19 +0100

    Update main.cpp

--- CPP_Module_06/ex00/main.cpp
+++ CPP_Module_06/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "ScalarConverter.hpp"
 #include <iostream>
 
+/* Be careful with numbers biiger than INT_MAX and smaller than INT_MIN in your following projects.  */
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <literal>" << std::endl;
