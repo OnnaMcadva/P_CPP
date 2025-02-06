3942b0d222041a250d00eaa6e74ab21e66ba3bfb
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-02-06 17:58:51 +0100

    Update iter.hpp

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -8,3 +8,10 @@ void iter(T *array, size_t length, void (*func)(T &)) {
         func(array[i]);
     }
 }
+
+template <typename T>
+void iter(T *array, size_t length, void (*func)(const T & )) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
